41543eb88afd862675dcf2270b89e9736381e5a7
Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>
2025-02-21 19:40:58 +0100

    Extend pages with Restaurant and Users displays

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,8 @@
 import { HomePage } from "./HomePage.jsx";
 import { LoginPage } from "./LoginPage.jsx";
 import { RegisterPage } from "./RegisterPage.jsx";
+import { RestaurantsPage } from "./RestaurantsPage.jsx";
+import { UsersPage } from "./UsersPage.jsx";
 
 /********************************************* Named exports (ES module) **********************************************/
-export { HomePage, LoginPage, RegisterPage };
+export { HomePage, LoginPage, RegisterPage, RestaurantsPage, UsersPage };
