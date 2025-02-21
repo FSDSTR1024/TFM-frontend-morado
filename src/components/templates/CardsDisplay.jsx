e92e98280ee83979611d89305c51d0bf9930ffd0
Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>
2025-02-21 19:28:01 +0100

    Refactor template

--- src/components/templates/CardsDisplay.jsx
+++ src/components/templates/CardsDisplay.jsx
@@ -23,58 +23,20 @@ const CardsDisplay = ({ CardComponent, cardProperties, filterMethod, headerSubti
   }, [filterMethod, itemsList, sortKey, sortOrder]);
 
   return (
-    <section className="p-6 bg-base-200 min-h-screen">
+    <section className="p-6 bg-base-200 min-h-screen flex flex-col items-center">
       {/* DisplayHeader */}
-      <div className="text-center mb-8">
+      <div className="text-center mb-4">
         <h1 className="text-4xl font-bold mb-2">{headerTitle}</h1>
         <p className="text-lg text-gray-600">
           {headerSubtitle} (total: {itemsList.length})
         </p>
       </div>
 
-      {/* DisplaySortingBtns */}
-      <div className="flex justify-center mb-6">
-        <div className="flex space-x-8 items-center">
-          <div className="flex space-x-4 items-center">
-            <label className="block text-sm font-medium text-gray-700">Sort By</label>
-            <select
-              className="mt-1 block pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-primary focus:border-primary sm:text-sm rounded-md"
-              onChange={handleSortKeyChange}
-              value={sortKey}
-            >
-              {cardProperties.map((cardProperty, index) => (
-                <option key={index} value={cardProperty.value}>
-                  {cardProperty.text}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className="flex space-x-4 items-center">
-            <label className="block text-sm font-medium text-gray-700">Order</label>
-            <select
-              className="mt-1 block pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-primary focus:border-primary sm:text-sm rounded-md"
-              onChange={handleSortOrderChange}
-              value={sortOrder}
-            >
-              <option value="ASC">Ascending</option>
-              <option value="DESC">Descending</option>
-            </select>
-          </div>
-        </div>
-      </div>
-
-      {/* DisplaySortingSubheader */}
-      {cardProperties.some((cardProperty) => cardProperty.filter && cardProperty.filter.value !== "") && (
-        <div className="text-center mb-2">
-          <h2 className="text-lg text-gray-600">Showing {sortedFilteredItems.length} filtered consumers:</h2>
-        </div>
-      )}
-
-      {/* FilteredSortedCards */}
-      <div className="flex justify-center">
+      {/* FiltersAndSortingsBtns */}
+      <div className="flex justify-center gap-32">
         {/* Filters */}
-        <aside className="mb-8 mr-10">
-          <div className="space-y-4">
+        <aside>
+          <div className="flex justify-center gap-5">
             {cardProperties.map((cardProperty) => {
               if (cardProperty.filter) {
                 return (
@@ -92,17 +54,56 @@ const CardsDisplay = ({ CardComponent, cardProperties, filterMethod, headerSubti
                   </div>
                 );
               }
+              return null;
             })}
           </div>
         </aside>
 
-        {/* Cards */}
-        <div className="flex flex-wrap justify-center gap-10 max-w-7xl">
-          {sortedFilteredItems &&
-            sortedFilteredItems.map((item) => {
-              return <CardComponent isTheNewest={item === newestItem} key={item._id} {...item} />;
-            })}
+        {/* DisplaySortingBtns */}
+        <div className="flex justify-center">
+          <div className="flex gap-5 items-center">
+            <div>
+              <label className="block text-sm font-medium text-gray-700">Sort By</label>
+              <select
+                className="mt-1 block pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-primary focus:border-primary sm:text-sm rounded-md"
+                onChange={handleSortKeyChange}
+                value={sortKey}
+              >
+                {cardProperties.map((cardProperty, index) => (
+                  <option key={index} value={cardProperty.value}>
+                    {cardProperty.text}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-gray-700">Order</label>
+              <select
+                className="mt-1 block pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-primary focus:border-primary sm:text-sm rounded-md"
+                onChange={handleSortOrderChange}
+                value={sortOrder}
+              >
+                <option value="ASC">Ascending</option>
+                <option value="DESC">Descending</option>
+              </select>
+            </div>
+          </div>
         </div>
+      </div>
+
+      {/* DisplaySortingSubheader */}
+      {cardProperties.some((cardProperty) => cardProperty.filter && cardProperty.filter.value !== "") && (
+        <div className="text-center mt-2">
+          <h2 className="text-lg text-gray-600">Showing {sortedFilteredItems.length} filtered consumers:</h2>
+        </div>
+      )}
+
+      {/* Cards */}
+      <div className="flex flex-wrap justify-center gap-10 max-w-7xl mt-6">
+        {sortedFilteredItems &&
+          sortedFilteredItems.map((item) => {
+            return <CardComponent isTheNewest={item === newestItem} key={item._id} {...item} />;
+          })}
       </div>
     </section>
   );
