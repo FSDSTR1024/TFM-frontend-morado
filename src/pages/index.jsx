3b6ef5fad1b91801e7b389bbcd1e465c6cd6c24e
Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>
2025-02-24 17:41:38 +0100

    Move pages to subfolders

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,9 @@
 /******************************************** Import all individual pages *********************************************/
-import { ConsumersPage } from "./ConsumersPage.jsx";
+import { ConsumersPage } from "./consumers/ConsumersPage.jsx";
 import { HomePage } from "./HomePage.jsx";
 import { LoginPage } from "./LoginPage.jsx";
 import { RegisterPage } from "./RegisterPage.jsx";
-import { RestaurantsPage } from "./RestaurantsPage.jsx";
+import { RestaurantsPage } from "./restaurants/RestaurantsPage.jsx";
 
 /********************************************* Named exports (ES module) **********************************************/
 export { ConsumersPage, HomePage, LoginPage, RegisterPage, RestaurantsPage };
