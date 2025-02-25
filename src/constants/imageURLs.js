baaa2c37f71a8abd555537b4ca6a6bc6935ba43d
Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>
2025-02-25 23:54:16 +0100

    Provide constants files for the default images URLs

---
+++ src/constants/imageURLs.js
@@ -0,0 +1,7 @@
+/*********************************************** Constants definitions ************************************************/
+/* Error images */
+const notActiveUserImgUrl = "https://res.cloudinary.com/dkpdltxzm/image/upload/ar_1:1,c_auto_pad,g_auto/v1739303950/uzhmdi16bdjvilwsaiy4.png";
+const notDefinedImgUrl = "https://res.cloudinary.com/dkpdltxzm/image/upload/ar_1:1,c_auto,g_auto/v1739315334/mw7prcgn3nykmaakze6b.png";
+
+/********************************************** Named export (ES module) **********************************************/
+export { notActiveUserImgUrl, notDefinedImgUrl };
---
+++ src/constants/index.js
@@ -0,0 +1,5 @@
+/******************************************** Import all individual pages *********************************************/
+import { notActiveUserImgUrl, notDefinedImgUrl } from "./imageURLs.js";
+
+/********************************************* Named exports (ES module) **********************************************/
+export { notActiveUserImgUrl, notDefinedImgUrl };
