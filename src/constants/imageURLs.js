f1d3e4ee09a6fcab20b915856424b918e7742e85
Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>
2025-03-02 00:12:30 +0100

    Add food allergens icons

--- src/constants/imageURLs.js
+++ src/constants/imageURLs.js
@@ -3,5 +3,23 @@
 const notActiveUserImgUrl = "https://res.cloudinary.com/dkpdltxzm/image/upload/ar_1:1,c_auto_pad,g_auto/v1739303950/uzhmdi16bdjvilwsaiy4.png";
 const notDefinedImgUrl = "https://res.cloudinary.com/dkpdltxzm/image/upload/ar_1:1,c_auto,g_auto/v1739315334/mw7prcgn3nykmaakze6b.png";
 
+/* Food allergy images */
+const foodAllergenImgUrls = {
+  celery: "https://res.cloudinary.com/dkpdltxzm/image/upload/v1740869868/pxuqkzi6kx2hja2ixaji.jpg",
+  crustaceans: "https://res.cloudinary.com/dkpdltxzm/image/upload/v1740869874/bpj5wvfuassbfh9oxwzq.jpg",
+  eggs: "https://res.cloudinary.com/dkpdltxzm/image/upload/v1740869881/yoeupe6cifei3rgkyo7t.jpg",
+  fish: "https://res.cloudinary.com/dkpdltxzm/image/upload/v1740869884/qvvrjumcg0xluiu89a3r.jpg",
+  gluten: "https://res.cloudinary.com/dkpdltxzm/image/upload/v1740870146/uew0t8p9kkimwgxsciuo.jpg",
+  lupins: "https://res.cloudinary.com/dkpdltxzm/image/upload/v1740870151/plymxdjjiaa9q1vdyqlj.jpg",
+  milk: "https://res.cloudinary.com/dkpdltxzm/image/upload/v1740870155/olxc3stym7oiy2lywvco.jpg",
+  mustard: "https://res.cloudinary.com/dkpdltxzm/image/upload/v1740870159/wv1iiqpn9fvihsj1xqh3.jpg",
+  nuts: "https://res.cloudinary.com/dkpdltxzm/image/upload/v1740870163/jovvjtjmrdx8wbilwum0.jpg",
+  peanuts: "https://res.cloudinary.com/dkpdltxzm/image/upload/v1740870176/ysroo8fxcx2mdes1edhh.jpg",
+  sesame: "https://res.cloudinary.com/dkpdltxzm/image/upload/v1740870221/lqmtwk2tnvkjomd8eist.jpg",
+  shellfish: "https://res.cloudinary.com/dkpdltxzm/image/upload/v1740870253/ifdvteuusqso9nt0tcx4.jpg",
+  soya: "https://res.cloudinary.com/dkpdltxzm/image/upload/v1740870257/qjvgfmorsej7sieoust6.jpg",
+  sulphites: "https://res.cloudinary.com/dkpdltxzm/image/upload/v1740870263/sudvbotykatvdoqqoxqx.jpg"
+};
+
 /********************************************** Named export (ES module) **********************************************/
-export { notActiveUserImgUrl, notDefinedImgUrl };
+export { foodAllergenImgUrls, notActiveUserImgUrl, notDefinedImgUrl };
