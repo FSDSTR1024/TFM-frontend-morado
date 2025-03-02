8e842256c53dd450d78e7504da76ee93ee6f6387
Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>
2025-03-02 16:10:03 +0100

    Adapt restaurant card with same details

--- src/components/atoms/RestaurantCard.jsx
+++ src/components/atoms/RestaurantCard.jsx
@@ -28,53 +28,57 @@ const RestaurantCard = ({ _id, description, img_url, is_activated, isTheNewest, 
   if (name.includes("u")) achievements.push(achievement4Url);
 
   return (
-    <div className="card w-72 bg-base-100 shadow-xl flex justify-between">
-      <div className="flex justify-center">
-        <button className="btn glass btn-block btn-outline btn-info btn-sm" onClick={handleOnCardClick}>View</button>
-      </div>
-      <div className="flex items-center my-4 mx-3 justify-between">
-        <div className="flex flex-col items-start">
-          {isTheNewest && <div className="badge badge-primary mb-2">NEW</div>}
-          <h2 className="card-title text-2xl">{name}</h2>
-          <p className="text-lg text-gray-600">{location}</p>
-        </div>
-        <img
-          alt={`${_id} restaurant profile picture`}
-          className="w-32 h-32 rounded-lg ml-6"
-          src={currentUserImgUrl}
-        />
-      </div>
-      {description && (
-        <div className="mx-3">
-          <p className="text-md text-gray-500">{description}</p>
-        </div>
-      )}
-      <div className="divider text-xl font-semibold mx-0 my-4">Details</div>
-      <div className="mx-4 my-0">
-        <div className="flex justify-between text-base">
-          <strong>Dishes:</strong>
-          <span>{nrOfDishes.toLocaleString("en-US")}</span>
-        </div>
-        <div className="flex justify-between items-center text-base">
-          <strong>Rating:</strong>
-          <StarRating _id={_id} nrOfReviews={nrOfReviews} rating={rating} starsSize="sm" textSize="sm" />
-        </div>
-        {web_page && (
-          <div className="flex justify-between text-base">
-            <strong>Website:</strong>
-            <a href={web_page} target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">{web_page}</a>
+    <div className="indicator">
+      <div className="card border border-base-300 w-72 bg-base-100 shadow-xl indicator">
+        {isTheNewest && <span className="indicator-item badge badge-primary font-semibold">NEW</span>}
+        <div className="card-body flex justify-between p-0 gap-0">
+          <div className="flex items-center my-4 mx-3 justify-between">
+            <div className="flex flex-col items-start">
+              <h2 className="card-title text-2xl">{name}</h2>
+              <p className="text-lg text-gray-600">{location}</p>
+            </div>
+            <img
+              alt={`${_id} restaurant profile picture`}
+              className="w-32 h-32 rounded-lg ml-6"
+              src={currentUserImgUrl}
+            />
           </div>
-        )}
-      </div>
-      <div className="mb-3">
-        <div className="divider text-xl font-semibold mt-6">Achievements</div>
-        {achievements && achievements.length > 0 && (
-          <div className="flex justify-center space-x-4 mt-2">
-            {achievements.map((achievement, index) => (
-              <img key={index} alt={`Achievement ${index + 1}`} className="w-12 h-12 rounded-full" src={achievement} />
-            ))}
+          {description && (
+            <div className="mx-3">
+              <p className="text-md text-gray-500">{description}</p>
+            </div>
+          )}
+          <div className="divider text-xl font-semibold mx-0 my-4">Details</div>
+          <div className="mx-4 my-0">
+            <div className="flex justify-between text-base">
+              <strong>Dishes:</strong>
+              <span>{nrOfDishes.toLocaleString("en-US")}</span>
+            </div>
+            <div className="flex justify-between items-center text-base">
+              <strong>Rating:</strong>
+              <StarRating _id={_id} nrOfReviews={nrOfReviews} rating={rating} starsSize="sm" textSize="sm" />
+            </div>
+            {web_page && (
+              <div className="flex justify-between text-base">
+                <strong>Website:</strong>
+                <a href={web_page} target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">{web_page}</a>
+              </div>
+            )}
           </div>
-        )}
+          <div className="mb-6">
+            <div className="divider text-xl font-semibold mt-6">Achievements</div>
+            {achievements && achievements.length > 0 && (
+              <div className="flex justify-center space-x-4 mt-2">
+                {achievements.map((achievement, index) => (
+                  <img key={index} alt={`Achievement ${index + 1}`} className="w-12 h-12 rounded-full" src={achievement} />
+                ))}
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+      <div className="indicator-item indicator-bottom indicator-center flex justify-center">
+        <button className="btn glass btn-outline btn-info btn-sm" onClick={handleOnCardClick}>View</button>
       </div>
     </div>
   );
