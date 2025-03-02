f4505faf8af89dae4b8798e0509e94f5d21c446c
Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>
2025-03-02 12:01:07 +0100

    Adapt consumer card

--- src/components/atoms/ConsumerCard.jsx
+++ src/components/atoms/ConsumerCard.jsx
@@ -1,6 +1,6 @@
 /*********************************************** External Node modules ************************************************/
 import { useCallback } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 /************************************************* Internal libraries *************************************************/
 import { notActiveUserImgUrl, notDefinedImgUrl } from "/src/constants";
@@ -28,15 +28,15 @@ const ConsumerCard = ({ _id, img_url, is_activated, isTheNewest, name, reviewed_
   if (name.includes("u")) achievements.push(achievement4Url);
 
   return (
-    <div className="card w-72 bg-base-100 shadow-xl">
+    <div className="card w-60 bg-base-100 shadow-xl flex justify-between">
       <div className="flex justify-center">
         <button className="btn glass btn-block btn-outline btn-info btn-sm" onClick={handleOnCardClick}>View</button>
       </div>
-      <div className="flex items-center p-2 pr-4 justify-between h-28">
+      <div className="flex items-center my-4 mx-2 justify-between">
         <img
-          src={roundImg({ imgURL: currentUserImgUrl })}
-          alt="Profile"
+          alt={`${_id} consumer profile picture`}
           className="w-20 h-20 rounded-full mr-6"
+          src={roundImg({ imgURL: currentUserImgUrl })}
         />
         <div className="flex flex-col items-end">
           {isTheNewest && <div className="badge badge-primary mb-2">NEW</div>}
@@ -45,26 +45,24 @@ const ConsumerCard = ({ _id, img_url, is_activated, isTheNewest, name, reviewed_
         </div>
       </div>
       <div className="divider text-xl font-semibold m-0">Reviews</div>
-      <div className="p-5 pt-2 pb-0">
-        <div className="flex justify-between text-base">
-          <strong>Restaurants:</strong>
-          <span>{reviewed_restaurants.toLocaleString("en-US")}</span>
-        </div>
+      <div className="m-3 mt-2 mb-0">
         <div className="flex justify-between text-base">
           <strong>Dishes:</strong>
           <span>{reviewed_dishes.toLocaleString("en-US")}</span>
         </div>
+        <div className="flex justify-between text-base">
+          <strong>Restaurants:</strong>
+          <span>{reviewed_restaurants.toLocaleString("en-US")}</span>
+        </div>
       </div>
-      <div className="pb-3">
-        <div className="divider text-xl font-semibold mt-6">Achievements</div>
-        {achievements && achievements.length > 0 ? (
+      <div className="mb-3">
+        <div className="divider text-xl font-semibold mt-4">Achievements</div>
+        {achievements && achievements.length > 0 && (
           <div className="flex justify-center space-x-4 mt-2">
             {achievements.map((achievement, index) => (
               <img key={index} alt={`Achievement ${index + 1}`} className="w-10 h-10" src={achievement} />
             ))}
           </div>
-        ) : (
-          <></>
         )}
       </div>
     </div>
