05613fd62eb24d937542d8f9d3e7edddb24c6bee
Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>
2025-03-02 12:01:39 +0100

    Update constant img url

--- src/constants/imageURLs.js
+++ src/constants/imageURLs.js
@@ -1,6 +1,6 @@
 /*********************************************** Constants definitions ************************************************/
 /* Error images */
-const notActiveUserImgUrl = "https://res.cloudinary.com/dkpdltxzm/image/upload/ar_1:1,c_auto_pad,g_auto/v1739303950/uzhmdi16bdjvilwsaiy4.png";
+const notActiveUserImgUrl = "https://res.cloudinary.com/dkpdltxzm/image/upload/ar_1:1,c_auto,g_auto/v1740871211/rikhjksebudyrm6gfdhf.png";
 const notDefinedImgUrl = "https://res.cloudinary.com/dkpdltxzm/image/upload/ar_1:1,c_auto,g_auto/v1739315334/mw7prcgn3nykmaakze6b.png";
 
 /* Food allergy images */
