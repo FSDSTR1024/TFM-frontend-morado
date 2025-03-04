76a9cdb900b8db426c37fe2cc9fe28121e1c108b
Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>
2025-03-04 15:02:06 +0100

    Update restaurant card with the "follow/following button"

--- src/components/atoms/RestaurantCard.jsx
+++ src/components/atoms/RestaurantCard.jsx
@@ -1,16 +1,33 @@
 /*********************************************** External Node modules ************************************************/
-import { useCallback } from "react";
+import { useCallback, useContext, useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 
 /************************************************* Internal libraries *************************************************/
+import { AuthContext } from "/src/contexts";
 import { getUserImgURL } from "/src/utils";
 import { StarRating } from "/src/components/atoms";
 
 /************************************************ Component Definition ************************************************/
-const RestaurantCard = ({ _id, description, img_url, is_activated, isTheNewest, location, name, nrOfDishes, nrOfReviews, rating, web_page }) => {
+const RestaurantCard = ({ _id, description, followers, img_url, is_activated, isTheNewest, location, name, nrOfDishes, nrOfReviews, rating, web_page }) => {
+  const { loggedUser } = useContext(AuthContext);
   const navigate = useNavigate();
 
+  const [isConsumer, setIsConsumer] = useState(false);
+  const [isFollowing, setIsFollowing] = useState(false);
+  useEffect(() => {
+    setIsConsumer(loggedUser?.role === "consumers");
+    setIsFollowing(true);
+  }, [loggedUser]);
+
+  const handleFollowClick = useCallback(() => {
+    navigate(`/restaurants/${_id}`);
+  }, [_id]);
+
   const handleOnCardClick = useCallback(() => {
+    navigate(`/restaurants/${_id}`);
+  }, [_id]);
+
+  const handleUnfollowClick = useCallback(() => {
     navigate(`/restaurants/${_id}`);
   }, [_id]);
 
@@ -75,7 +92,20 @@ const RestaurantCard = ({ _id, description, img_url, is_activated, isTheNewest, 
           </div>
         </div>
       </div>
-      <div className="indicator-item indicator-bottom indicator-center flex justify-center">
+      <div className="indicator-item indicator-bottom indicator-center flex justify-center gap-7">
+        {isConsumer && (
+          isFollowing ? (
+            <button className="btn glass btn-outline btn-error btn-sm" onClick={handleUnfollowClick}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" className="size-[1.2em]"><path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" /></svg>
+              Following
+            </button>
+          ) : (
+            <button className="btn glass btn-outline btn-error btn-sm" onClick={handleFollowClick}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" className="size-[1.2em]"><path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" /></svg>
+              Follow
+            </button>
+          )
+        )}
         <button className="btn glass btn-outline btn-info btn-sm" onClick={handleOnCardClick}>View</button>
       </div>
     </div>
