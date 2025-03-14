41fd7a6de03dc3db3308d1e9d6fbb3a11d0dee91
Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>
2025-03-14 22:40:48 +0100

    Make image in footer bigger

--- src/components/atoms/Footer.jsx
+++ src/components/atoms/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
           <div className="w-full md:w-2/3 mb-6 md:mb-0 text-center">
             <img
               alt="Nyam! application logo"
-              className="mx-auto mb-4 h-14"
+              className="mx-auto mb-4 h-32"
               src={NyamLogo}
             />
             <p className="text-gray-600">A TFM project developed by Martí Planagumà Solà.</p>
