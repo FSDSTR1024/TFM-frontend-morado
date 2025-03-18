defd5da3203a09f8b3e27cebc67cf4a84f9ecfe8
Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>
2025-03-18 23:14:47 +0100

    REVERT unfinished follow functionality

--- src/components/atoms/RestaurantCard.jsx
+++ src/components/atoms/RestaurantCard.jsx
@@ -91,51 +91,6 @@ const RestaurantCard = ({ _id, description, email, followers, img_url, is_activa
         </div>
       </div>
       <div className="indicator-item indicator-bottom indicator-center flex justify-center gap-7">
-        {isConsumer && (
-          isFollowing ? (
-            <button
-              className="btn glass btn-outline btn-error btn-sm flex items-center gap-1"
-              onClick={handleUnfollowClick}
-            >
-              <svg
-                className="size-[1.2em]"
-                fill="currentColor"
-                stroke="currentColor"
-                strokeWidth="2.5"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-              <span>Following</span>
-            </button>
-          ) : (
-            <button
-              className="btn glass btn-outline btn-error btn-sm flex items-center gap-1"
-              onClick={handleUnfollowClick}
-            >
-              <svg
-                className="size-[1.2em]"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2.5"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-              <span>Follow</span>
-            </button>
-          )
-        )}
         <button className="btn glass btn-outline btn-info btn-sm" onClick={handleOnCardClick}>View</button>
       </div>
     </div>
