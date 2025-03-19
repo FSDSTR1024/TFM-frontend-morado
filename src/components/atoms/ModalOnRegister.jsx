58e676fd526005ab6551b7d47c0f074af3c78299
Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>
2025-03-19 19:46:00 +0100

    REFACTOR on register modal

--- src/components/atoms/ModalOnRegister.jsx
+++ src/components/atoms/ModalOnRegister.jsx
@@ -7,9 +7,9 @@ const ModalOnRegister = () => {
           {/* if there is a button in form, it will close the modal */}
           <button className="btn btn-sm btn-circle btn-outline btn-error absolute right-2 top-2">✕</button>
         </form>
-        <h3 className="font-bold text-lg">📧 Almost there!</h3>
-        <p className="pt-4">You're just one step away.</p>
-        <p>Please check your email and confirm your registration to complete the process.</p>
+        <h3 className="font-bold text-lg">📧 Almost there! You're just one step away.</h3>
+        <p className="pt-4">We have sent an email to the provided email.</p>
+        <p>Please check your mail box and confirm your registration by pressing the button in it.</p>
       </div>
     </dialog>
   );
