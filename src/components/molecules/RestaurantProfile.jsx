ea3ee9a1144932073262317b3583146371f4cda1
Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>
2025-03-26 01:02:00 +0100

    ADD followers into restaurant profile page

--- src/components/molecules/RestaurantProfile.jsx
+++ src/components/molecules/RestaurantProfile.jsx
@@ -137,7 +137,14 @@ const RestaurantProfile = ({ restaurantId }) => {
                 <h1 className="text-4xl font-bold">{restaurant.name}</h1>
                 <StarRating {...restaurant} />
               </div>
-              <p className="text-lg text-gray-600 mb-2">📍 {restaurant.location}</p>
+              <div className="flex justify-between items-center gap-2">
+                <p className="text-lg text-gray-600 mb-2">📍 {restaurant.location}</p>
+                <div className="flex gap-1 items-center text-error">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" className="size-[1.2em]"><path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" /></svg>
+                  <span>{restaurant.followers.length.toLocaleString("en-US")}</span>
+                  <span>follower{restaurant.followers.length !== 1 && "s"}</span>
+                </div>
+              </div>
               {restaurant.description && (
                 <div className="mb-2 text-xl text-gray-400 italic">
                   <TextParagraph text={restaurant.description} />
